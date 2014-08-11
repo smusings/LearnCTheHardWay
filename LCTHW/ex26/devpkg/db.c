90d7e8441b207113104e573b504af1c20e672900
smusings <etyulmenkov@yahoo.com>
2014-08-11 16:45:16 -0400

    ex29 added

--- LCTHW/ex26/devpkg/db.c
+++ LCTHW/ex26/devpkg/db.c
@@ -42,14 +42,19 @@ error:
 	if(data) bdestroy(data);
 	return NULL;
 }
+
+//updates the database
 int DB_update(const char *url){
+	//if file is present, then it says so
 	if(DB_find(url)){
 		log_info("Already recorded as installed: %s", url);
 	}
 	
+	//checks for file
 	FILE *db=DB_open(DB_FILE, "a+");
 	check(db, "Failed to open DB file: %s", DB_FILE);
 	
+	//writes the line into the db, if failed gives error back
 	bstring line=bfromcstr(url);
 	bconchar(line, '\n');
 	int rc=fwrite(line->data, blength(line), 1, db);
@@ -61,7 +66,8 @@ error:
 	return -1;
 }
 
-
+//searched the database for the url
+//gives error if the database did not have the file
 int DB_find(const char *url){
 	bstring data=NULL;
 	bstring line=bfromcstr(url);
@@ -83,11 +89,13 @@ error: //fallthrough
 	return res;
 }
 
+//initilized the database
 int DB_init(){
 	apr_pool_t *p=NULL;
 	apr_pool_initialize();
 	apr_pool_create(&p, NULL);
 	
+	//access the database and makes a new directory
 	if(access(DB_DIR, W_OK | X_OK)==-1){
 		apr_status_t rc=apr_dir_make_recursive(DB_DIR,
 			APR_UREAD | APR_UWRITE | APR_UEXECUTE |
